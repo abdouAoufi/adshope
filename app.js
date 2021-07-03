a91467da7d1a2f9efcd721c93a3f4907b61278e0
Abderahmane Aoufi <abdouou7@gmail.com>
2021-07-03 22:48:11 +0100

    add express

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require("http");
-const routes = require('./routes');
 
 
 const host = "127.0.0.1";
 const port = 8080;
 
-const server = http.createServer(routes);
+const server = http.createServer();
 
 server.listen(port, host, () => {
   console.log("server is running on "+host+":"+port);
