9d162021620304023f84c7d7db982830f64e5fd7
Abderahmane Aoufi <abdouou7@gmail.com>
2021-07-04 09:19:04 +0100

    first look express

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const http = require("http");
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-const host = "127.0.0.1";
-const port = 8080;
-
-const server = http.createServer();
-
-server.listen(port, host, () => {
-  console.log("server is running on "+host+":"+port);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action='/product' method="POST">
+      <input type='text' name='title'/>
+      <input type='number' name='quantity'/>
+      <button type='submit'>Add product </button>
+    </form>`
+  );
 });
+
+// we add another middleware to handle another request
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>hello from express </h1>");
+});
+
+app.listen(3000);
