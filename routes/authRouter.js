bfbef1320486ddf5abe98109792554b9dc4b82b9
AbdouAoufi <Abdouou7@gmail.com>
2021-08-04 23:28:49 +0100

    sending password reset

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,13 +5,21 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 // ? GET REQUEST LOGIN
 router.get("/login", authController.getLogin);
-// ? POST REQUEST LOGIN 
+// ? POST REQUEST LOGIN
 router.post("/login", authController.postLogin);
 // ? POST REQUEST
-router.post("/logout" , authController.postLogout)
+router.post("/logout", authController.postLogout);
 // ? GET REQUEST
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 // ? POST REQUEST
-router.post('/signup', authController.postSignup);
+router.post("/signup", authController.postSignup);
+// ? GET REQUEST
+// ? POST REQUEST
+router.post("/reset", authController.postReset);
+// ? GET REQUEST
+router.get("/reset/:token" , authController.getNewPassword);
+// ? POST REQUESR
+router.post("/new-password" , authController.postNewPassword)
+
 
 module.exports = router;
